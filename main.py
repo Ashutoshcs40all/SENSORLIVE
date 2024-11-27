2794350ace8fb1ed9530b7efed2f960a88d25af3
Ashutoshcs40all <ashutoshmaurya063@gmail.com>
2024-11-27 18:00:36 +0530

    Data Ingestion commit

---
+++ main.py
@@ -0,0 +1,36 @@
+from sensor.configuration.mongo_db_connection import MongoDBClient
+from sensor.exception import SensorException
+import os , sys
+from sensor.logger import logging
+#from sensor.utils import dump_csv_file_to_mongodb_collection
+
+from sensor.pipeline.training_pipeline import TrainPipeline
+
+
+# def test_exception():
+#     try:
+#         logging.info("ki yaha p bhaiaa ek error ayegi diveision by zero wali error ")
+#         a=1/0
+#     except Exception as e:
+#        raise SensorException(e,sys) 
+      
+
+if __name__ == "__main__":
+    
+    
+    # file_path="aps_failure_training_set1.csv"
+    # database_name="ineuron"
+    # collection_name ="sensor"
+    # dump_csv_file_to_mongodb_collection(file_path,database_name,collection_name)
+
+    training_pipeline = TrainPipeline()
+    training_pipeline.run_pipeline()
+
+
+
+# if __name__ == "__main__":
+
+#     try:
+#         test_exception()
+#     except Exception as e:
+#         print(e)
--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
-from setuptools import find_packages, setup
-#from typing import List
+from setuptools import find_packages,setup
+from typing import List
 
-def get_requirements()->list[str]:
+def get_requirements()->List[str]:
+    """
 
-    reuirements_list : list[str] =[]
 
-    return reuirements_list
+    This function will return list of requirements
+    """
+    requirement_list:List[str] = []
+
+    """
+    Write a code to read requirements.txt file and append each requirements in requirement_list variable.
+    """
+    return requirement_list
 
 
 setup(
