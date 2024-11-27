81798a80eb704725cededef40fe036cd11f0e370
Ashutoshcs40all <ashutoshmaurya063@gmail.com>
2024-11-27 23:59:35 +0530

    validation till

--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -1,30 +1,33 @@
-# from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig,DataTransformationConfig
-# from sensor.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact,DataTransformationArtifact
-# from sensor.entity.artifact_entity import ModelEvaluationArtifact,ModelPusherArtifact,ModelTrainerArtifact
-# from sensor.entity.config_entity import ModelPusherConfig,ModelEvaluationConfig,ModelTrainerConfig
-
-
-# from sensor.exception import SensorException
-# import sys,os
-# from sensor.logger import logging
-# from sensor.components.data_ingestion import DataIngestion
-# from sensor.components.data_validation import DataValidation
-# from sensor.components.data_transformation import DataTransformation
-# from sensor.components.model_trainer import ModelTrainer
-# from sensor.components.model_evaluation import ModelEvaluation
-# from sensor.components.model_pusher import ModelPusher
-
-# from sensor.constant.training_pipeline import SAVED_MODEL_DIR
-
-
-# from sensor.cloud_storage.s3_syncer import S3Sync
-# from sensor.constant.s3_bucket import TRAINING_BUCKET_NAME
 from sensor.entity.config_entity import TrainingPipelineConfig ,DataIngestionConfig
 from sensor.exception  import SensorException
 from sensor.entity.artifact_entity import DataIngestionArtifact
 from sensor.logger import logging
 import sys , os 
 from sensor.components.data_ingestion import DataIngestion
+
+
+from sensor.components.data_validation import DataValidation
+
+from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig
+
+from sensor.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
+
+
+from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig,DataTransformationConfig
+
+
+from sensor.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact,DataTransformationArtifact
+
+from sensor.components.data_transformation import DataTransformation
+
+
+# from sensor.constant.training_pipeline import SAVED_MODEL_DIR
+
+
+# from sensor.cloud_storage.s3_syncer import S3Sync
+# from sensor.constant.s3_bucket import TRAINING_BUCKET_NAME
+
+
 
 
 class TrainPipeline:
@@ -60,37 +63,37 @@ class TrainPipeline:
 
 
 
-    # def start_data_validaton(self,data_ingestion_artifact:DataIngestionArtifact)->DataValidationArtifact:
+    def start_data_validaton(self,data_ingestion_artifact:DataIngestionArtifact)->DataValidationArtifact:
         
-    #     try:
-    #         data_validation_config = DataValidationConfig(training_pipeline_config=self.training_pipeline_config)
+        try:
+            data_validation_config = DataValidationConfig(training_pipeline_config=self.training_pipeline_config)
 
-    #         data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
-    #         data_validation_config = data_validation_config
-    #         )
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+            data_validation_config = data_validation_config
+            )
 
-    #         data_validation_artifact = data_validation.initiate_data_validation()
+            data_validation_artifact = data_validation.initiate_data_validation()
 
-    #         return data_validation_artifact
+            return data_validation_artifact
         
-    #     except  Exception as e:
-    #         raise  SensorException(e,sys)
+        except  Exception as e:
+            raise  SensorException(e,sys)
 
 
 
 
 
 
-    # def start_data_transformation(self,data_validation_artifact:DataValidationArtifact):
-    #     try:
-    #         data_transformation_config = DataTransformationConfig(training_pipeline_config=self.training_pipeline_config)
-    #         data_transformation = DataTransformation(data_validation_artifact=data_validation_artifact,
-    #         data_transformation_config=data_transformation_config
-    #         )
-    #         data_transformation_artifact =  data_transformation.initiate_data_transformation()
-    #         return data_transformation_artifact
-    #     except  Exception as e:
-    #         raise  SensorException(e,sys)
+    def start_data_transformation(self,data_validation_artifact:DataValidationArtifact):
+        try:
+            data_transformation_config = DataTransformationConfig(training_pipeline_config=self.training_pipeline_config)
+            data_transformation = DataTransformation(data_validation_artifact=data_validation_artifact,
+            data_transformation_config=data_transformation_config
+            )
+            data_transformation_artifact =  data_transformation.initiate_data_transformation()
+            return data_transformation_artifact
+        except  Exception as e:
+            raise  SensorException(e,sys)
         
 
 
@@ -169,11 +172,11 @@ class TrainPipeline:
             data_ingestion_artifact:DataIngestionArtifact = self.start_data_ingestion()
 
 
-            # data_validation_artifact=self.start_data_validaton(data_ingestion_artifact=data_ingestion_artifact)
+            data_validation_artifact=self.start_data_validaton(data_ingestion_artifact=data_ingestion_artifact)
 
 
 
-            # data_transformation_artifact = self.start_data_transformation(data_validation_artifact=data_validation_artifact)
+            data_transformation_artifact = self.start_data_transformation(data_validation_artifact=data_validation_artifact)
 
 
             # model_trainer_artifact = self.start_model_trainer(data_transformation_artifact)
